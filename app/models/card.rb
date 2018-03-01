919ef98ebc42f166ccd86805e36eddfb4c478d99
Alan <alin@2u.com>
2018-03-01 08:59:05 -0500

    add model spec for card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ApplicationRecord
+  validates_presence_of :front, :back
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+      front: 'test-front',
+      back: 'test-back',
+    )
+   }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a front" do
+    subject.front = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a back" do
+    subject.back = nil
+    expect(subject).to_not be_valid
+  end
 end
