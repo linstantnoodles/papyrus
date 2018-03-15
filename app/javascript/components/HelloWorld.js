383a851c1fbdc7fff35d5497a6e0c0852ef46706
Alan <alin@2u.com>
2018-03-15 00:03:19 -0400

    react redux

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
-class HelloWorld extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
-    );
-  }
-}
-
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
-export default HelloWorld
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../reducers'
+import App from './App'
+ 
+const store = createStore(rootReducer)
+ 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
