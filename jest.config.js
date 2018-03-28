74467c4ad1076eef44148fed1e4ebdf67fff2dcd
Alan <alin@2u.com>
2018-03-28 01:35:49 -0400

    add first test

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleDirectories: [
+    "node_modules",
+    "app/javascript/"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/config/webpack/",
+    "/vendor/bundle/"
+  ]
+};
