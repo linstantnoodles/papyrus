504001a1bce616f2412696ecaa1cfec854e86949
Alan <alin@2u.com>
2018-07-22 01:40:52 -0400

    Remove series page

--- app/controllers/brand/pages_controller.rb
+++ app/controllers/brand/pages_controller.rb
@@ -4,12 +4,6 @@ module Brand
 
     def about; end
 
-    def series
-      @posts = Post.all.order('published_at DESC').select do |post|
-        post.published? && post.series?
-      end
-    end
-
     def til
       @posts = Post.all.order('published_at DESC').select do |post|
         post.published? && post.tagged_with?('til')
